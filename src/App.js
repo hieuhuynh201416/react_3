23f2784b09ae21838d895fe6c75150b19a86ff45
hieuhuynh <hieuhuynh201416@gmail.com>
2023-11-25 16:08:25 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Ex_Shoe from './Ex_Shoe/Ex_Shoe';
+
+function App() {
+  return (
+    <div className="App">
+      <Ex_Shoe/>
+    </div>
+  );
+}
+
+export default App;
